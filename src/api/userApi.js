ee3df6ba5a5cae388e9136ad7e8d54d78e4b6059
jroger <jroger@expertime.com>
2016-12-07 14:48:57 +0100

    Mock - Add delete method

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,6 +7,19 @@ export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/{${id}`);
+}
+
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
+}
+
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
 }
