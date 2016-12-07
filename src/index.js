b771b8fc0fac0de24af332507887d186ea52503d
jroger <jroger@expertime.com>
2016-12-07 14:32:26 +0100

    mock JSON Server added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+import {getUsers} from './api/userApi'
+
 import './index.css'
 
-import numeral from 'numeral'
+getUsers().then(result => {
+  let userBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00')
-console.log(`I would pay ${courseValue} for this awesome course!`) // eslint-disable-line
+  result.forEach(user => {
+    userBody+= `
+      <tr>
+          <td> <a href="#" data-id="${user.id}">Delete</a></td>
+          <td>${user.id}</td>
+          <td>${user.firstName}</td>
+          <td>${user.lastName}</td>
+          <td>${user.email}</td>
+      </tr>`
+  })
+
+  global.document.getElementById('users').innerHTML = userBody;
+})
