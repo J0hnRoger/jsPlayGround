ee5ef440b66eb86f7be665376d0a516c85644de8
jroger <jroger@expertime.com>
2016-12-13 13:49:30 +0100

    split in vendor and main JS

---
+++ src/vendor.js
@@ -0,0 +1,6 @@
+/* This file contains references to the vendor libraries we are using in this project.
+This is used by webpack in production mode only. */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch'
